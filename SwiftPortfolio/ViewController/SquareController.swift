021dca3bbe80e8511a9b6bd963433705c190be14
Colin Szeto <kolinszeto@gmail.com>
2020-03-11 09:21:09 -0700

    Added Square Storyboard

---
+++ SwiftPortfolio/ViewController/SquareController.swift
@@ -0,0 +1,51 @@
+//
+//  SquareController.swift
+//  SwiftPortfolio
+//
+//  Created by Szeto, Colin on 3/11/20.
+//  Copyright © 2020 Szeto, Colin. All rights reserved.
+//
+
+import UIKit
+class SquareController: UIViewController {
+    
+    // IBOutlet connects to Storyboard items, I had problems with Drag
+    
+    @IBOutlet weak var side1: UITextField!
+    @IBOutlet weak var side2: UITextField!
+    @IBOutlet weak var area: UILabel!
+    @IBOutlet weak var perimeter: UILabel!
+
+    
+    // Loads at initialization
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize Outlets
+        side1.text = "0.0"
+        side2.text = "0.0"
+        area.text = "0.0"
+        perimeter.text = "0.0"
+
+    }
+
+    // IBAction created with Drag from Storyboard
+   @IBAction func calculate(sender: UIButton) {
+        // let is for values that are not modified
+        let s1 = Float(side1.text!) // Input value, !(Bang) avoids errors
+        let s2 = Float(side2.text!)
+
+        // Area
+        let a1 = s1! * s2!
+    
+    //Perimeter calculation
+        let p1 = (s1! *2) + ( 2 * s2!)
+    // Set calculation to screen
+        //side3.text = String(s3)
+       // print("Pyth  \(side3.text!)")
+    area.text = String(a1)
+    perimeter.text = String(p1)
+
+    
+    }
+}
