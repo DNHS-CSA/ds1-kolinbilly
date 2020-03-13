3604c97c67c39d183177eccf71628b9b4620c49b
Colin Szeto <kolinszeto@gmail.com>
2020-03-13 09:20:21 -0700

    added some clarifying comments

--- SwiftPortfolio/ViewController/DistController.swift
+++ SwiftPortfolio/ViewController/DistController.swift
@@ -9,8 +9,8 @@
 import UIKit
 class DistController: UIViewController {
     
-    // IBOutlet connects to Storyboard items, I had problems with Drag
-    
+    // IBOutlet connects to Storyboard items, I had problems with ctrl drag, note that start with items on storyboard, then drag them into the .swft file
+    //all of the labels
     @IBOutlet weak var input: UITextField!
     @IBOutlet weak var kilometer: UITextField!
     @IBOutlet weak var meter: UITextField!
@@ -28,33 +28,31 @@ class DistController: UIViewController {
     // Loads at initialization
     override func viewDidLoad() {
         super.viewDidLoad()
-        let dist = ("0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0")
+        let dist = ("0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0","0.0") // the labels to 0.0
         
         //initilize outlets
         input.text = (dist.0)//this is inches
-        kilometer.text = (dist.1)
-        meter.text = (dist.2)
-        centimeter.text = (dist.3)
-        millimeter.text = (dist.4)
-        micrometer.text = (dist.5)
-        mile.text = (dist.6)
-        yard.text = (dist.7)
-        foot.text = (dist.8)
-        inch.text = (dist.0)
-        nauticalMile.text = (dist.9)
-        nanoMeter.text = (dist.10)
+        kilometer.text = (dist.1) //setting label = 0.0
+        meter.text = (dist.2)//setting label = 0.0
+        centimeter.text = (dist.3)//setting label = 0.0
+        millimeter.text = (dist.4)//setting label = 0.0
+        micrometer.text = (dist.5)//setting label = 0.0
+        mile.text = (dist.6)//setting label = 0.0
+        yard.text = (dist.7)//setting label = 0.0
+        foot.text = (dist.8)//setting label = 0.0
+        inch.text = (dist.0)//setting label = 0.0
+        nauticalMile.text = (dist.9)//setting label = 0.0
+        nanoMeter.text = (dist.10)//setting label = 0.0
 
     }
 
     // IBAction created with Drag from Storyboard
    @IBAction func calculate(sender: UIButton) {
         
-       let inpt1 = Float(input.text!) //this is for the
+       let inpt1 = Float(input.text!) //this is to allow the string to be used in the calculations
 
-
-    
         // Area
-        let a1 = inpt1! /  39370 //inch tokm
+        let a1 = inpt1! /  39370 //inch to km
         let a2 = inpt1! /  39.37 //inch to meter
         let a3 = inpt1! *  2.54 //inch to cm
         let a4 = inpt1! *  25.4 //inch to mm
@@ -64,11 +62,9 @@ class DistController: UIViewController {
         let a7 = a8 / 3 //ft to yrd
         let a6 = a7 / 1760 //yrd to mile
         let a9 = a6 / 1.151 //mile to natuical mile
-        let a11 = inpt1!
+        let a11 = inpt1! //1:1 conversion for inches
         
     // Set calculation to screen
-        //side3.text = String(s3)
-       // print("Pyth  \(side3.text!)")
     inch.text = String(a11)
     kilometer.text = String(a1)
     meter.text = String(a2)
